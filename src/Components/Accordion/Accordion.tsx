a3ccc4e6fc02ed303520d45294b67bcc5702d163
Alexei-Karamzin <1999aleks_1999@mail.ru>
2022-03-04 13:39:47 +0300

    first commit

---
+++ src/Components/Accordion/Accordion.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AccordionTitle } from "./AccordionTitle";
+import { Accordionbudy } from "./Accordionbudy";
+
+export function Accordion() {
+    return(
+        <div>
+            <AccordionTitle />
+            <Accordionbudy />
+        </div>
+    )
+}
---
+++ src/Components/Accordion/AccordionTitle.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AccordionTitle(){
+    return <div>
+        <h1>title</h1>
+    </div>
+}
